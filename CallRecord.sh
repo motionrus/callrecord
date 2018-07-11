255d33d7f83e785dc7d19bdaabcbb22fdb7752d6
motionrus <tytin@narod.ru>
2018-07-11 11:15:19 +0300

    create archive files recursive

--- CallRecord.sh
+++ CallRecord.sh
@@ -2,16 +2,16 @@
 #
 # Created by Ruslan Tyutin
 # 
-# count Hours in year/2 is (365/2)*24 = 4380 hours
-#
+# Count Hours in year/2 is (365/2)*24 = 4380 hours
 # COUNT_FILES="4380"
 # COUNT_SECONDS="3600"
+# if CallRecord (dumpcap) is not running, then start the process
 
 
-COUNT_FILES="10"
-COUNT_SECONDS="4"
+COUNT_FILES="4380"
+COUNT_SECONDS="3600"
 CALLRECORD_PATH="/tmp/call_record/call_record.pcapng"
-CALLRECORD_HOST="not host 1.1.1.1"
+CALLRECORD_HOST="port 1719|5061"
 CALLRECORD_INTERFACE="enp3s0"
 
 let count_running_procces=$(ps aux | grep call_record | awk ' { print $2 } ' | wc -l)-1
---
+++ compression.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2.7
+
+# Compress   : Created by Ruslan Tyutin
+# Description: The script runs through all files with the specified extension.
+#              Archives and deletes the original files, except the last.
+#              To work, you must specify the full path and file extension for archiving
+
+import os
+
+FULL_PATH = '/home/rus/PycharmProjects/callrecord/callrecord_init/call_record'
+EXTENSION_FILE = 'pcapng'
+
+
+def compress(path, mask=''):
+    os.chdir(path)
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    if mask != '':
+        match_list = [x for x in files if x.endswith(mask)]
+        files = match_list
+    for f in sorted(files)[:-1]:
+        os.system('tar -cjf %s %s' % (f + '.tar.bz2', f))
+        os.system('rm ' + f)
+
+
+if __name__ == '__main__':
+    compress(FULL_PATH, EXTENSION_FILE)
